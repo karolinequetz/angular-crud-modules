41cc7d73ff3f7e081d19e064052828853e85b4ca
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-10-28 20:50:35 -0300

    feat: created students list component

--- src/app/features/students/students.module.ts
+++ src/app/features/students/students.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentsRoutingModule } from './students-routing.module';
+import { StudentListComponent } from './student-list/student-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StudentListComponent
+  ],
   imports: [
     CommonModule,
     StudentsRoutingModule
