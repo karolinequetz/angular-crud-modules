cdca15312b77d817222fca8c781c26e3bda420ce
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-10-28 20:51:37 -0300

    feat: created students new component

---
+++ src/app/features/students/student-new/student-new.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-new',
+  templateUrl: './student-new.component.html',
+  styleUrls: ['./student-new.component.css']
+})
+export class StudentNewComponent {
+
+}
--- src/app/features/students/students.module.ts
+++ src/app/features/students/students.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentListComponent } from './student-list/student-list.component';
 import { StudentEditComponent } from './student-edit/student-edit.component';
+import { StudentNewComponent } from './student-new/student-new.component';
 
 
 @NgModule({
   declarations: [
     StudentListComponent,
-    StudentEditComponent
+    StudentEditComponent,
+    StudentNewComponent
   ],
   imports: [
     CommonModule,
