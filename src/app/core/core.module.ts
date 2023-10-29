2190aa99920df45e5455d02388eb1494ad334952
Karoline de Souza Quetz <kquetz@viceri.com.br>
2023-10-29 18:40:06 -0300

    feat: created json server

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+import { MainComponent } from './layout/main/main.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule, NgbModule],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, MainComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CommonModule,
+    RouterModule,
+    NgbModule,
+    NgxSpinnerModule,
+  ],
+  exports: [MainComponent],
 })
 export class CoreModule {}
